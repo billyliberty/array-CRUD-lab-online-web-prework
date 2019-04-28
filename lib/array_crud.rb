eee166d635c1da0a4eca36868dea78ff30860cb7
Joshua Crum <joshua.crum@gmail.com>
2019-04-28 16:16:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  newArray = ["dog", "cat", "mouse"]
+  create_an_array = ["dog", "cat", "mouse", "shark"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -19,7 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  element = remove_element_from_start_of_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
